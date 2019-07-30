3225c147d0111ddfad35a734d12323fa51e914ce
Martijn Dorsman <mdorsman@flaire.nl>
2019-07-30 20:07:44 +0200

    Finished selection card

--- src/app/boat-selection/selection-card/selection-card.component.ts
+++ src/app/boat-selection/selection-card/selection-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-selection-card',
@@ -7,7 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectionCardComponent implements OnInit {
 
-  constructor() { }
+  harbors = [
+    {
+      id: 0,
+      name: 'Amsterdam',
+    },
+    {
+      id: 1,
+      name: 'Rotterdam'
+    },
+    {
+      id: 2,
+      name: 'Giethoorn'
+    }
+  ];
+
+  adults = Array(8).fill(0).map((x, i) => 8 - i);
+  children = Array(8).fill(0).map((x, i) => 8 - i);
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
